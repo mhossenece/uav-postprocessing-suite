2e2d8b9c6d934e3fc6b71549c001e7d867cf9a68
mhossenece <143030953+mhossenece@users.noreply.github.com>
2025-05-22 20:30:25 -0400

    Update extract_last_download.m

--- extract_last_download.m
+++ extract_last_download.m
@@ -12,8 +12,7 @@
 %   - Output: 'download.txt'
 %
 % Author: Md Sharif Hossen  
-% PhD Student, Department of Electrical and Computer Engineering, NCSU  
-% Advisors: Dr. Ismail Guvenc and Dr. Vijay K. Shah  
+% PhD Student, Department of Electrical and Computer Engineering, NCSU   
 % Date: May 4, 2025
 %
 % Copyright (c) 2025 Md Sharif Hossen  
